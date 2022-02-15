faffd8852a717d7de6a1ade66e089f6642514cbd
sudonnoh <sudonnoh@gmail.com>
2022-02-15 16:58:30 +0900

    update

--- python/pyqt5.py
+++ python/pyqt5.py
@@ -22,7 +22,7 @@ class MainApp(QMainWindow):
         # window 위치 및 크기
         self.center()
         self.width = 300
-        self.height = 100
+        self.height = 200
         self.setFixedSize(self.width, self.height)
         
         # 작업 APP
@@ -45,6 +45,12 @@ class SubApp(QWidget):
         
     def initUI(self):
         # widgets
+        self.img_label = QLabel('')
+        self.change_btn = QPushButton('Image Update')
+        self.update_btn = QPushButton('Image Update')
+        self.change_btn.setMaximumWidth(100)
+        self.update_btn.setMaximumWidth(100)
+        
         self.label = QLabel('File: ')
         self.lineEdit = QLineEdit()
         self.Add_btn = QPushButton('File Add')
@@ -54,11 +60,25 @@ class SubApp(QWidget):
         
         # Add_btn Click
         self.Add_btn.clicked.connect(self.add_open)
+        self.Ok_btn.clicked.connect(self.add_ok)
+        self.change_btn.clicked.connect(self.change_open)
+        self.update_btn.clicked.connect(self.img_update)
         
         # layout
         vbox = QVBoxLayout()
         hbox = QHBoxLayout()
         hbox2 = QHBoxLayout()
+        hbox3 = QHBoxLayout()
+        hbox4 = QHBoxLayout()
+        
+        hbox3.addStretch(1)
+        hbox3.addWidget(self.img_label)
+        hbox3.addStretch(1)
+        
+        hbox4.addStretch(1)
+        hbox4.addWidget(self.change_btn)
+        hbox4.addWidget(self.update_btn)
+        hbox4.addStretch(1)
         
         hbox.addStretch(1)
         hbox.addWidget(self.label)
@@ -70,17 +90,38 @@ class SubApp(QWidget):
         hbox2.addWidget(self.Ok_btn)
         hbox2.addStretch(1)
         
-        vbox.addStretch(2)
+        vbox.addStretch(1)
+        vbox.addLayout(hbox3)
+        vbox.addLayout(hbox4)
+        vbox.addStretch(1)
         vbox.addLayout(hbox)
         vbox.addLayout(hbox2)
         vbox.addStretch(1)
         
         self.setLayout(vbox)
         
+    def change_open(self):
+        
+        urls, _ = QFileDialog.getOpenFileNames(
+            caption='Select one or more files to change',
+            directory='./',
+            filter="image(*.jpg *.jpeg *.png)"
+        )
+        self.img_label.setText("Ready to Change Image.")
+        # 여기 할 차례 urls 를 목록으로 보여주기 or get해서 img_update로 보내기
+        
+    def img_update(self):
+        
+        print(self.FileOpen[0])
+        
     def add_open(self):
         
         FileOpen = QFileDialog.getOpenFileName(self, 'Open File', './')
         self.lineEdit.setText(FileOpen[0])
+        
+    def add_ok(self):
+        
+        print(self.lineEdit.text())
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
